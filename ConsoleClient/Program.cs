4e8289893069eb8a9cc631e641d87ad4df5393be
cwelt <36764366+cwelt@users.noreply.github.com>
2020-05-14 13:24:42 +0300

    Layer out the strategy design pattern class structure for the melody composition service

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
