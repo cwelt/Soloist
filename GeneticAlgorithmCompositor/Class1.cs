4bfedf02d3e2e7419b3f163cafd857f277a516ad
cwelt <chwelt88@gmail.com>
2020-05-14 14:05:14 +0300

    Move the genetic algorithm compositor to a separate assembly & layer out it's algoritm's skeleton using the template method design pattern

---
+++ GeneticAlgorithmCompositor/Class1.cs
@@ -0,0 +1,51 @@
+﻿using CW.Soloist.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW.Soloist.GeneticAlgorithmCompositor
+{
+    public abstract class GeneticAlgorithmCompositor : CompositorStrategy
+    {
+        public override object Compose(string playback)
+        {
+            // get first generatiion 
+            InitializeFirstGeneration();
+
+            int i = 0;
+            bool terminateCondition = false;
+
+            while (!terminateCondition)
+            {
+                // mix and combine pieces of different individuals 
+                Crossover();
+
+                // modify parts of individuals 
+                Mutate();
+
+                // rate each individual 
+                EvaluateFitness();
+
+                // natural selection 
+                SelectNextGeneration();
+                //MelodyGenome.CurrentGeneration++;
+
+                if (i++ == 100)
+                    terminateCondition = true;
+            }
+
+            // TODO: convert internal genome representation of each candidate in to a MIDI track chunk representation
+            return null;
+
+        }
+
+
+        protected internal abstract void InitializeFirstGeneration(); 
+        protected internal abstract void Crossover(); 
+        protected internal abstract void Mutate(); 
+        protected internal abstract void EvaluateFitness(); 
+        protected internal abstract void SelectNextGeneration(); 
+    }
+}
