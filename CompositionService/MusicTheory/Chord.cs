040cf231f6f98b4920c29d2e6dfa8563b7d4d4f0
cwelt <chwelt88@gmail.com>
2020-05-17 16:01:48 +0300

    add an chord interface and parial implemetation

---
+++ CompositionService/MusicTheory/Chord.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW.Soloist.CompositionService.MusicTheory
+{
+    internal class Chord : IChord
+    {
+        public NoteName ChordRoot { get; }
+        public ChordType ChordType { get; }
+        public INoteDuration Duration { get; set; }
+
+
+        /// <summary>
+        /// Construct a new <see cref="IChord"/> instance with the given
+        /// <paramref name="root"/>, <paramref name="type"/> and <paramref name="duration"/>. 
+        /// </summary>
+        /// <param name="root"> The name of the note which is the chord's root. </param>
+        /// <param name="type"> The type of the chord (<see cref="ChordType"/>).</param>
+        /// <param name="duration"> The Duration of the chord (<see cref="INoteDuration"/>).</param>
+        public Chord(NoteName root, ChordType type, INoteDuration duration)
+        {
+            ChordRoot = root;
+            ChordType = type;
+            Duration = duration;
+        }
+
+        public IEnumerable<NotePitch> GetNotes(int minOctave, int maxOctave)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ CompositionService/MusicTheory/IChord.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW.Soloist.CompositionService.MusicTheory
+{
+    /// <summary>
+    /// Represents a single musical chord instance. 
+    /// <para>
+    /// Each chord is represented by a chord root, a chord type and a chord duration.
+    /// </para>
+    /// </summary>
+    internal interface IChord
+    {
+        /// <summary> Name of the note which is the root of the chord. </summary>
+        NoteName ChordRoot { get; }
+
+        /// <summary> Type of the chord (<see cref="ChordType"/>) </summary>
+        ChordType ChordType { get; }
+
+        /// <summary> The chords's duration (<see cref="INoteDuration"/>) .</summary>
+        INoteDuration Duration { get; set; }
+
+
+        /// <summary>
+        /// Returns an enumerator that can be used to iterate through the collection 
+        /// of all note pitches which correspond to this chord's root and type, 
+        /// and fullfill the requested octave range constraint. 
+        /// </summary>
+        /// <param name="minOctave"> Minimum octave for note pitches.</param>
+        /// <param name="maxOctave"> Maximum octave for note pitches</param>
+        /// <returns>
+        /// An enumerator that can be used to iterate through the collection of notes in the chord.
+        /// Only note pitches which are within the requested range are enumerated. 
+        /// </returns>
+        IEnumerable<NotePitch> GetNotes(int minOctave, int maxOctave);
+    }
+}
