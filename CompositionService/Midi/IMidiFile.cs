f179a0b1188bf8f2c16829eec3db740f9b0fa2e9
cwelt <chwelt88@gmail.com>
2020-05-20 18:56:56 +0300

    define IMidiFile, a midi file high level interface

---
+++ CompositionService/Midi/IMidiFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW.Soloist.CompositionService.Midi
+{
+    interface IMidiFile
+    {
+
+        string Title { get; set; }
+        int BeatsPerMinute { get; set; }
+        int NumberOfBars {get; set; }
+        ICollection<object> Tracks { get; set; }
+        ICollection<object> Instruments { get; set; }
+
+        void Play();
+        void Stop();
+
+    }
+}
