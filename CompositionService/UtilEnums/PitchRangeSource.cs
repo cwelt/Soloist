a58f7e619d8dfc2e20e7ad060f7fc17c225619c5
cwelt <chwelt88@gmail.com>
2020-06-16 12:36:30 +0300

    Add Pitch Range Source Enum.

---
+++ CompositionService/UtilEnums/PitchRangeSource.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace CW.Soloist.CompositionService.UtilEnums
+{
+    /// <summary>
+    /// Source for determining the pitch value range for the composition - 
+    /// either the user's custom selection of lower and upper bound parameters,
+    /// or the range from the exisiting melody in the midi file, if such melody exists.
+    /// </summary>
+    public enum PitchRangeSource
+    {
+        /// <summary> 
+        /// Pitch range would be determined according to the existing lowest 
+        /// and highest pitches in the existing melody in the midi file.
+        /// </summary>
+        [Description("Midi File Melody Track Range")]
+        MidiFile,
+
+        /// <summary>
+        /// Pitch range would be determined by the user's custom selection.
+        /// </summary>
+        [Description("Custom Range Selecton")]
+        Custom
+    }
+}
