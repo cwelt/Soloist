164fb5ee6f4de340c115e8ad367f940fc35c2ac0
cwelt <chwelt88@gmail.com>
2020-06-21 22:05:45 +0300

    Add Basic form for composition parameters

--- WebApplication/Controllers/CompositionController.cs
+++ WebApplication/Controllers/CompositionController.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using CW.Soloist.CompositionService;
+using CW.Soloist.CompositionService.Compositors;
+using CW.Soloist.CompositionService.Midi;
+using CW.Soloist.CompositionService.MusicTheory;
+using CW.Soloist.CompositionService.UtilEnums;
+using CW.Soloist.WebApplication.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +17,31 @@ namespace CW.Soloist.WebApplication.Controllers
         // GET: Composition
         public ActionResult Index()
         {
-            return View();
+            CompositionParamsViewModel viewModel = new CompositionParamsViewModel
+            {
+                MusicalInstrument = MusicalInstrument.AcousticGrandPiano,
+                OverallNoteDurationFeel = OverallNoteDurationFeel.Extreme,
+                MinPitch = NotePitch.E2,
+                MaxPitch = NotePitch.E7
+            };
+            return View(viewModel);
+        }
+        [HttpPost]
+        public FileResult Compose(CompositionParamsViewModel model)
+        {
+            var chordFilePath = this.HttpContext.Server.MapPath("/SampleData/twenty_years_chords.txt");
+            var midiFilePath = this.HttpContext.Server.MapPath("/SampleData/after_20_years.mid");
+
+            Composition composition = new Composition(chordFilePath, midiFilePath, 1);
+            IMidiFile midiFile = composition.Compose(
+                strategy: CompositionStrategy.ArpeggioScaleMixStrategy,
+                overallNoteDurationFeel: model.OverallNoteDurationFeel,
+                musicalInstrument: model.MusicalInstrument,
+                minPitch: model.MinPitch,
+                maxPitch: model.MaxPitch);
+
+            midiFile.PlayAsync();
+            return null;
         }
     }
 }
---
+++ WebApplication/ViewModels/CompositionParamsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CW.Soloist.CompositionService.Midi;
+using CW.Soloist.CompositionService.MusicTheory;
+using CW.Soloist.CompositionService.UtilEnums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CW.Soloist.WebApplication.ViewModels
+{
+    public class CompositionParamsViewModel
+    {
+        [Required]
+        [DisplayName("Overall Note Duration Feel")]
+        public OverallNoteDurationFeel OverallNoteDurationFeel { get; set; }
+
+
+        [Required]
+        [DisplayName("Musical Instrument")]
+        public MusicalInstrument MusicalInstrument { get; set; }
+
+        [DisplayName("Pitch Lowest Bound")]
+        public NotePitch MinPitch { get; set; }
+
+        [DisplayName("Pitch Upper Bound")]
+        public NotePitch MaxPitch { get; set; }
+
+    }
+}
