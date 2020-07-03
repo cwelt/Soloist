f2286d8c7681c13b5f3c578e4135dbafb045180c
cwelt <chwelt88@gmail.com>
2020-07-03 17:03:41 +0300

    Install Autofac for DI (Dependency Injection) Managment

--- WebApplication/Global.asax.cs
+++ WebApplication/Global.asax.cs
@@ -1,3 +1,6 @@
+using Autofac;
+using Autofac.Integration.Mvc;
+using CW.Soloist.Data.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +19,43 @@ namespace WebApplication
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            #region Autofac DI Configurations 
+            // create an autofac container builder
+            ContainerBuilder builder = new ContainerBuilder();
+
+            // Register MVC controllers (MvcApplication is the name of the class in Global.asax.)
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // OPTIONAL: Register model binders that require DI.
+            builder.RegisterModelBinders(typeof(MvcApplication).Assembly);
+            builder.RegisterModelBinderProvider();
+
+            // OPTIONAL: Register web abstractions like HttpContextBase.
+            builder.RegisterModule<AutofacWebTypesModule>();
+
+            // OPTIONAL: Enable property injection in view pages.
+            builder.RegisterSource(new ViewRegistrationSource());
+
+            // OPTIONAL: Enable property injection into action filters.
+            builder.RegisterFilterProvider();
+
+            // OPTIONAL: Enable action method parameter injection (RARE).
+            //builder.InjectActionInvoker();
+
+            // manual registration of types and services;
+            builder.RegisterType<SongRepository>().As<ISongRepostiory>();
+            //builder.RegisterType<ApplicationDbContext>();
+
+            // For property injection using Autofac
+            // builder.RegisterType<TheRequestedService>().PropertiesAutowired();
+
+            // build the dependencies container 
+            IContainer container = builder.Build();
+
+            // Set the dependency resolver of MVC to be Autofac.
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            #endregion
         }
     }
 }
