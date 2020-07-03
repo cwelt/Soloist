f6031848c0405b08405b96022ea8c1445c17a085
cwelt <chwelt88@gmail.com>
2020-07-03 15:20:39 +0300

    Add a Song Domain Model

---
+++ Data/Models/Song.cs
@@ -0,0 +1,20 @@
+﻿using CW.Soloist.CompositionService.Midi;
+using CW.Soloist.CompositionService.MusicTheory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW.Soloist.Data.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public IMidiFile Midi { get; set; }
+        public IEnumerable<IChord> Chords { get; set; }
+    }
+}
