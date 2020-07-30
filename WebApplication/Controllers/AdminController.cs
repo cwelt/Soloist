8d2d5b6b36f1018db223cc3bbbe71029148393dc
cwelt <chwelt88@gmail.com>
2020-07-30 12:36:41 +0300

    Add Admin controller for application management

---
+++ WebApplication/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CW.Soloist.WebApplication.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FileServer()
+        {
+            string fileServerAppDataUrl = ConfigurationManager.AppSettings["FileServerAppDataUrl"];
+            return Redirect(fileServerAppDataUrl);
+        }
+    }
+}
