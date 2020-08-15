877e22f3bda564efc799ea392ca0f1cf9a0f49f0
cwelt <chwelt88@gmail.com>
2020-08-16 00:20:15 +0300

    Update Duration Documentation

--- CompositionService/MusicTheory/Duration.cs
+++ CompositionService/MusicTheory/Duration.cs
@@ -6,20 +6,10 @@ using System.Threading.Tasks;
 
 namespace CW.Soloist.CompositionService.MusicTheory
 {
-    /// <summary>
-    /// Represents a duration of a musical component such as a note or chord. 
-    /// </summary>
-    /// <remarks>
-    /// Each duration is assembled of two properties: <see cref="Numerator"/>, 
-    /// which represents the number of beats in the duration, 
-    /// and <see cref="Denominator"/>, which represents the duration of a single beat,
-    /// in relation to whole note duration. For example, a chord which is played 
-    /// over half a bar, would have a duration of 1/2. Duration of notes and chords 
-    /// are calculated as the quotient of the nominator divided by the denominator, 
-    /// so for example, a duration instance of 2/4 is equivalent to 1/2. 
-    /// </remarks>
+    /// <inheritdoc cref="IDuration"/>
     internal class Duration : IDuration
     {
+        #region Duration Constants
         internal const float WholeNoteFraction = 1F;
         internal const float HalfNoteFraction = 0.5F;
         internal const float QuaterNoteFraction = 0.25F;
@@ -36,15 +26,22 @@ namespace CW.Soloist.CompositionService.MusicTheory
         internal const byte ThirtySecondNoteDenominator = 32;
         internal const byte TripletEighthNoteDenominator = 12;
         internal const byte TripletSixteenthNoteDenominator = 24;
+        #endregion
 
+
+        #region Properties
         /// <summary> Number of beats </summary>
         public byte Numerator { get; set; }
+
 
         /// <summary> Duration of a single beat </summary>
         public byte Denominator { get; set; }
 
+
         /// <inheritdoc cref="IDuration.Fraction"/>
         public float Fraction => (float)Numerator / Denominator;
+        #endregion
+
 
         #region Constructors
         /// <summary>
@@ -53,6 +50,7 @@ namespace CW.Soloist.CompositionService.MusicTheory
         /// </summary>
         /// <param name="numerator"></param>
         /// <param name="denominator"></param>
+        /// <param name="reduceToLowestTerms"></param>
         public Duration(byte numerator = 1, byte denominator = 4, bool reduceToLowestTerms = true)
         {
             if (reduceToLowestTerms)
@@ -72,25 +70,25 @@ namespace CW.Soloist.CompositionService.MusicTheory
         /// Copy constructor: constructs a <see cref="IDuration"/> instance 
         /// based on the <paramref name="duration"/> parameter values.
         /// </summary>
-        /// <param name="duration"></param>
+        /// <param name="duration"> The duration to base the values on. </param>
+        /// <param name="reduceToLowestTerms"> Reduces the numerator and denominator do their lowest terms. </param>
         public Duration(IDuration duration, bool reduceToLowestTerms = true)
             : this(duration.Numerator, duration.Denominator, reduceToLowestTerms) { }
         #endregion
 
 
-        public IDuration Subtract(IDuration duration)
-        {
-            return MusicTheoryServices.DurationAritmetic(MusicTheoryServices.AritmeticOperation.Subtract, this, duration)
-                .ReduceFractionToLowestTerms();
-        }
-
+        #region IDuration Methods
         public IDuration Add(IDuration duration)
         {
             return MusicTheoryServices.DurationAritmetic(MusicTheoryServices.AritmeticOperation.Add, this, duration)
                 .ReduceFractionToLowestTerms();
         }
 
-        public override string ToString() => $"{Numerator}/{Denominator}";
+        public IDuration Subtract(IDuration duration)
+        {
+            return MusicTheoryServices.DurationAritmetic(MusicTheoryServices.AritmeticOperation.Subtract, this, duration)
+                .ReduceFractionToLowestTerms();
+        }
 
         public bool IsDenominatorPowerOfTwo()
         {
@@ -111,7 +109,26 @@ namespace CW.Soloist.CompositionService.MusicTheory
             return (int)Math.Ceiling(Math.Log(Denominator) / Math.Log(2))
                 == (int)Math.Floor(Math.Log(Denominator) / Math.Log(2)); */
         }
+        #endregion
 
+
+        #region Additional Methods
+        /// <summary> Returns string representation of this duration. </summary>
+        /// <returns>A string representation of this duration. </returns>
+        public override string ToString() => $"{Numerator}/{Denominator}";
+
+
+        /// <summary> Utility function for calculating gcd with euclids'es algorithm. </summary>
+        /// <returns> The greatest common divisor of <paramref name="a"/> and <paramref name="b"/>.</returns>
+        internal static int GreatestCommonDivisor(int a, int b)
+        {
+            if (b == 0)
+                return a;
+            return GreatestCommonDivisor(b, a % b);
+        }
+        #endregion
+
+        #region Operators
         public static bool operator <= (Duration duration1, IDuration duration2)
         {
             return duration1.Fraction <= duration2.Fraction;
@@ -131,13 +148,6 @@ namespace CW.Soloist.CompositionService.MusicTheory
         {
             return duration1.Fraction > duration2.Fraction;
         }
-
-        //  function for calculating gcd with euclids'es algorithm
-        internal static int GreatestCommonDivisor(int a, int b)
-        {
-            if (b == 0)
-                return a;
-            return GreatestCommonDivisor(b, a % b);
-        }
+        #endregion
     }
 }
--- CompositionService/MusicTheory/IDuration.cs
+++ CompositionService/MusicTheory/IDuration.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CW.Soloist.CompositionService.MusicTheory
+﻿namespace CW.Soloist.CompositionService.MusicTheory
 {
-    /// <summary>
-    /// Represents a duration of a musical component such as a note or chord. 
-    /// </summary>
-    /// <remarks>
+    /// <summary> <para>
+    /// Represents a duration of a musical component such as a note or chord. </para>
     /// Each duration is assembled of two properties: <see cref="Numerator"/>, 
     /// which represents the number of beats in the duration, 
     /// and <see cref="Denominator"/>, which represents the duration of a single beat,
@@ -17,8 +9,7 @@ namespace CW.Soloist.CompositionService.MusicTheory
     /// over half a bar, would have a duration of 1/2. Duration of notes and chords 
     /// are calculated as the quotient of the nominator divided by the denominator, 
     /// so for example, a duration instance of 2/4 is equivalent to 1/2. 
-    /// </remarks>
-
+    /// </summary>
     public interface IDuration
     {
         /// <summary> Number of beats in the duration. </summary>
@@ -28,13 +19,11 @@ namespace CW.Soloist.CompositionService.MusicTheory
         byte Denominator { get; set; }
 
         /// <summary>
-        /// Subtracts the given duration from this duration instance and returns 
-        /// the difference length between them as a new duration. 
+        /// Length of the duration as a fraction that is the quotient of the division
+        /// of the duration's numerator by the duration's denominator. 
+        /// For exmaple, a duration of 1/4 has a fraction of 0.25.
         /// </summary>
-        /// <remarks> The argument's length must be equal or shorter than this duration's length. </remarks>
-        /// <param name="duration"> The duration to subtract from this duration. </param>
-        /// <returns> The new duration which is the result of the subtraction. </returns>
-        IDuration Subtract(IDuration duration);
+        float Fraction { get; }
 
         /// <summary>
         /// Adds the given duration to this duration instance and returns 
@@ -45,14 +34,18 @@ namespace CW.Soloist.CompositionService.MusicTheory
         IDuration Add(IDuration duration);
 
         /// <summary>
-        /// Length of the duration as a fraction. 
-        /// For exmaple, a duration of 1/4 has a fraction of 0.25.
+        /// Subtracts the given duration from this duration instance and returns 
+        /// the difference length between them as a new duration. 
         /// </summary>
-        float Fraction { get; }
+        /// <remarks> The argument's length must be equal or shorter than this duration's length. </remarks>
+        /// <param name="duration"> The duration to subtract from this duration. </param>
+        /// <returns> The new duration which is the result of the subtraction. </returns>
+        IDuration Subtract(IDuration duration);
 
-        /// <summary>
-        /// Utility method which determins whether this duration's 
+        /// <summary> 
+        /// Utility method which determines whether this duration's 
         /// denominator is a power of two or not.
+        /// This could be helpful for musical quantization process.
         /// </summary>
         /// <returns> True if this duration's denominator is a power of two, and false elsewise.</returns>
         bool IsDenominatorPowerOfTwo();
