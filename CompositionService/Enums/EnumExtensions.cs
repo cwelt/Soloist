d669d710e11164106c9f78eff787c306e1df48ec
cwelt <chwelt88@gmail.com>
2020-09-18 16:16:34 +0300

    Fix reference bug in views

--- CompositionService/Enums/EnumExtensions.cs
+++ CompositionService/Enums/EnumExtensions.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace CW.Soloist.CompositionService.Enums
 {
